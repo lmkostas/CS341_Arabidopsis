a1a98cb93ada1af22c67b968d6524f843ccee891
Lindsey Kostas <lindseymak@Lindseys-iMac.local>
2017-04-12 06:00:15 -0700

    updated preprocess to limit 100k and added doc to sentence implementation

--- split_docs.py
+++ split_docs.py
@@ -1,14 +1,24 @@
+'''
+This file takes a tsv file containing a series of pmc papers in where
+each paper is represented on a single line in the format of [pmc_id <tab> document text]
+and stores each doc-id, senetence tuple in each document in a postgress db, snorkel-small-data
+or snorkel-full-data, depending on the dataset being used
+
+The script takes one argument:
+1)'small' or 'full' to flag whether a subset or the full set of articles, respectively, is being processed.
+e.g. run split_docs.py <small/full>
+''' 
+
 import os
 import sys
+#top-level snorkel folder must be in the same directory as the project folder for this to run
 sys.path.insert(1, '../snorkel')
 
 def docs_to_sentences(data_size):
-	# TO USE A DATABASE OTHER THAN SQLITE, USE THIS LINE
-	# Note that this is necessary for parallel execution amongst other things...
 	db_name = 'full-data'
 	if data_size == 'small':
 		db_name = 'small-data'
-	#need this line for parallel processing
+	#need this line for parallel processing, cannot use sqlite db for this
 	os.environ['SNORKELDB'] = 'postgres:///snorkel-'+db_name
 
 	from snorkel import SnorkelSession
