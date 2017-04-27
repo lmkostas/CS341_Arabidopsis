c56e301f5ed3ea005933f0ce694ceb3e89a97672
Lindsey Kostas <lindseymak@Lindseys-iMac.local>
2017-04-27 01:15:18 -0700

    working on matchers

--- revised_extraction.py
+++ revised_extraction.py
@@ -32,7 +32,6 @@ def main():
     from snorkel.matchers import DictionaryMatch
     genes = load_gene_list()
     GM = DictionaryMatch(d=genes)
-
     phenos = load_pheno_list()
     PM = DictionaryMatch(d=phenos, attrib='lemmas')
 '''
@@ -92,7 +91,7 @@ def load_pheno_list():
     result.extend(util.read_tsv_flat(PHENO_EQ_LIST, delimiter=";"))
     result.extend(util.read_file_lines(PHENO_MANUAL))
 
-    result.extend(load_pheno_ontology())
+    #result.extend(load_pheno_ontology())
 
     # Filter by blacklist
     blacklist = read_blacklist()
@@ -105,6 +104,9 @@ def load_pheno_ontology():
     ontology_terms = []
     for ontology_file in ONTOLOGIES:
         ontology_terms.extend(parse_ontology(ontology_file))
+
+    blacklist = read_blacklist()
+    return [pheno.lower() for pheno in ontology_terms if pheno.lower() not in blacklist]
 
     return ontology_terms
 
@@ -149,17 +151,27 @@ def parse_pato(ontology_file):
 #    main()
 from snorkel import SnorkelSession
 session = SnorkelSession()
-from snorkel.matchers import Sequence, DictionaryMatch, Concat, RegexMatchSpan, SlotFillMatch, Union
+from snorkel.matchers import Sequence, DictionaryMatch, Concat, RegexMatchSpan, SlotFillMatch, Union, RegexMatchEach
 
 genes = load_gene_list()
-FULL_GENE = Union(DictionaryMatch(d=genes, longest_match_only=True), RegexMatchSpan(rgx=r'[A-Za-z]\w*-\d+'))
+FULL_GENE = Union(DictionaryMatch(d=genes, longest_match_only=True), RegexMatchEach(rgx=r'[A-Za-z]\w*-\d+'), longest_match_only=True)
 GM = Sequence(FULL_GENE, longest_match_only=True)
-
+GM = Concat(DictionaryMatch(d=genes, longest_match_only=True), RegexMatchSpan(rgx=r'-\d+'), longest_match_only=True, right_required=False)
 dict_linkwords = ['of', 'over', 'in', 'the', 'with', 'to', 'a']
 #adjs = ['advanced', 'reduced', 'greater', 'less', 'small', 'large', 'short', 'tall', 'increased', 'decreased']
-patos = parse_pato(PATO_ONTOLOGY)
-phenos = load_pheno_list()
+patos = parse_pato(PATO_ONTOLOGY) + ['increased', 'decreased', 'levels']
+phenos = load_pheno_list() #+ ['root']
+obos = load_pheno_ontology() + ['root']
 
-QUALIFIERS = RegexMatchSpan(rgx=r'JJ.*|JJS.*|JJR.*', attrib='pos_tags')
+QUALIFIERS = RegexMatchEach(rgx=r'JJ.*|JJS.*|JJR.*', attrib='pos_tags')
+#QUALIFIERS = RegexMatchSpan(rgx=r'JJ.*|JJS.*|JJR.*|VBN.*|RB.*|RBS.*|RBR.*|(JJ |JJS |JJR |VBN )?(NN|NNS|NNP|NNPS).*', attrib='pos_tags')
 LINKS = DictionaryMatch(d=dict_linkwords, longest_match_only=True)
-PM = Sequence(DictionaryMatch(d=phenos, longest_match_only=True, attrib='lemmas'), DictionaryMatch(d=patos, attrib='lemmas', longest_match_only=True), LINKS, QUALIFIERS, longest_match_only=True, required=[1, 0, 0, 0])
+PATOS = DictionaryMatch(d=patos, attrib='lemmas', longest_match_only=True)
+OBOS = DictionaryMatch(d=obos, attrib='lemmas', longest_match_only=True)
+PHENOS = DictionaryMatch(d=phenos, attrib='lemmas', longest_match_only=True)
+
+OBO_QUAL = Sequence(QUALIFIERS, PATOS, OBOS, LINKS, longest_match_only=True, required=[1, 0, 1, 0])
+#OBO_PATO = Sequence(QUALIFIERS, PATOS, OBOS, LINKS, longest_match_only=True, required = [1, 1, 1, 0])
+PM = Union(OBO_QUAL, PHENOS, longest_match_only=True)
+#PM = RegexMatchSpan(rgx=r'JJ.*|JJS.*|JJR.*', attrib='pos_tags')
+#PM = Sequence(DictionaryMatch(d=phenos, longest_match_only=True, attrib='lemmas'), DictionaryMatch(d=patos, attrib='lemmas', longest_match_only=True), LINKS, QUALIFIERS, longest_match_only=True, required=[1, 0, 0, 0])
