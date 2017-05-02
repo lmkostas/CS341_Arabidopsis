7775d1d982cecdb010f01b6f58e536e08c81bf00
Lindsey Kostas <lindseymak@Lindseys-iMac.local>
2017-05-02 13:18:47 -0700

    matchers

--- extended_matchers.py
+++ extended_matchers.py
@@ -90,8 +90,9 @@ class DictionaryMatch(NgramMatcher):
         self.ignore_case = self.opts.get('ignore_case', True)
         self.attrib      = self.opts.get('attrib', WORDS)
         self.reverse     = self.opts.get('reverse', False)
+        self.blacklist  =self.opts.get('blacklist', None)
         try:
-            self.d = frozenset(w.lower() if self.ignore_case else w for w in self.opts['d'])
+            self.d = frozenset(w.lower() if self.ignore_case else w for w in self.opts['d'] if self.blacklist is None or w not in self.blacklist)
         except KeyError:
             raise Exception("Please supply a dictionary (list of phrases) d as d=d.")
 
@@ -101,7 +102,15 @@ class DictionaryMatch(NgramMatcher):
         if self.stemmer is not None:
             if self.stemmer == 'porter':
                 self.stemmer = PorterStemmer()
-            self.d = frozenset(self._stem(w) for w in list(self.d))
+            if self.blacklist: self.blacklist = [self._stem(b) for b in self.blacklist]
+            temp = []
+            for w in list(self.d):
+                if len(w.split()) == 0 and (self.blacklist is None or self_stem(w) not in self.blacklist):
+                    temp.append(self._stem(w))
+                else:
+                    temp.append(w)
+            self.d = frozenset(temp)
+            #self.d = frozenset(self._stem(w) for w in list(self.d))
 
     def _stem(self, w):
         """Apply stemmer, handling encoding errors"""
@@ -224,6 +233,7 @@ class RegexMatch(NgramMatcher):
         self.ignore_case = self.opts.get('ignore_case', True)
         self.attrib      = self.opts.get('attrib', WORDS)
         self.sep         = self.opts.get('sep', " ")
+        self.blacklist  = self.opts.get('blacklist', None)
 
         # Compile regex matcher
         # NOTE: Enforce full span matching by ensuring that regex ends with $!
@@ -244,7 +254,7 @@ class RegexMatchEach(RegexMatch):
     """Matches regex pattern on **each token**"""
     def _f(self, c):
         tokens = c.get_attrib_tokens(self.attrib)
-        return True if tokens and all([self.r.match(t) is not None for t in tokens]) else False
+        return True if tokens and all([self.r.match(t) is not None for t in tokens]) else False    
 
 
 class PersonMatcher(RegexMatchEach):
@@ -328,7 +338,8 @@ class Sequence(NgramMatcher):
     """Matches a slot fill pattern of matchers _at the character level_"""
     def init(self):
         self.attrib = self.opts.get('attrib', WORDS)
-        self.sep = self.opts.get('sep', ' ')
+        self.sep            = self.opts.get('sep', ' ')
+        self.links            = self.opts.get('links', None)
         self.required = self.opts.get('required', [1 for _ in range(len(self.children))])
 
         # Check for correct number of child matchers / slots
@@ -336,14 +347,13 @@ class Sequence(NgramMatcher):
             raise ValueError("Number of provided matchers is 0.")
 
     def f(self, c):
-
         # First, filter candidates by matching splits pattern
         match_instance = [(r+1)%2 for r in self.required]
         m = re.match(r'([\w-]+)', c.get_attrib_span(self.attrib))
         if m is None:
             return False
         # Then, recursively apply matchers
-        for match in re.finditer(r'([\w-]+)', c.get_attrib_span(self.attrib)):
+        for match in re.finditer(r'([A-Za-z0-9-]+)', c.get_attrib_span(self.attrib)):
             match_found = False
             split = c.get_attrib_span(self.attrib)[match.start(1):match.end(1)]
             for i in range(len(self.children)):
@@ -351,6 +361,153 @@ class Sequence(NgramMatcher):
                     match_found = True
                     match_instance[i] = 1
                     break
+            '''
+            if not match_found and self.sep:
+                for matcher in self.sep:
+                    if matcher.f(c[match.start(1):match.end(1)]) != 0:
+                        match_found = True
+            '''
             if not match_found and (split != 'and' and split != 'or' and split != 'and/or'): return False
         if sum(match_instance) != len(match_instance): return False
         return True
+
+class Contains(NgramMatcher):
+    """Matches a slot fill pattern of matchers _at the character level_"""
+    def init(self):
+        try:
+            self.rgxs = self.opts['rgxs']
+        except KeyError:
+            raise Exception("Please supply a regular expression string r as rgx=r.")
+        self.ignore_case = self.opts.get('ignore_case', True)
+        self.attrib      = self.opts.get('attrib', [WORDS]*len(self.rgxs))
+        self.sep         = self.opts.get('sep', " ")
+        self.type =self.opts.get('type', 'and')
+
+        # Compile regex matcher
+        # NOTE: Enforce full span matching by ensuring that regex ends with $!
+        self.rgxs = [rgx if rgx.endswith('$') else rgx + r'$' for rgx in self.rgxs]
+        self.r = [re.compile(rgx, flags=re.I if self.ignore_case else 0) for rgx in self.rgxs]
+
+        # Check for correct number of child matchers / slots
+        if len(self.children) != 1:
+            raise ValueError("Number of provided matchers is not 1.")
+
+    def f(self, c):
+        # First, filter candidates by matching splits pattern
+        for child in self.children:
+            if child.f(c) <= 0: return False
+
+        if self.type =='or':
+            for i, rgx in enumerate(self.r):
+                if re.search(rgx, c.get_attrib_span(self.attrib[i], sep=self.sep)) is not None:
+                    return True
+
+            return False
+        else:
+            for i, rgx in enumerate(self.r):
+                if re.search(rgx, c.get_attrib_span(self.attrib[i], sep=self.sep)) is None:
+                    return False 
+
+            return True
+
+"""
+class Contains(NgramMatcher):
+    '''Matches a slot fill pattern of matchers _at the character level_'''
+    def init(self):
+        '''
+        try:
+            self.rgxs = self.opts['rgxs']
+        except KeyError:
+            raise Exception("Please supply a regular expression string r as rgx=r.")
+        '''
+        try:
+            self.query = self.opts['query']
+        except KeyError:
+            raise Exception("Please supply a query q as query=q.")
+        try:
+            self.type = self.opts['type']
+        except KeyError:
+            raise Exception("Please supply a type for query t as type=t.")
+        self.ignore_case = self.opts.get('ignore_case', True)
+        self.attrib      = self.opts.get('attrib', WORDS)
+        self.sep         = self.opts.get('sep', " ")
+        self.reverse    =self.opts.get('reverse', False)
+
+        # Compile regex matcher
+        # NOTE: Enforce full span matching by ensuring that regex ends with $!
+        if self.type == 'rgx':
+            self.rgxs = [rgx if rgx.endswith('$') else rgx + r'$' for rgx in self.query]
+            self.r = [re.compile(rgx, flags=re.I if self.ignore_case else 0) for rgx in self.rgxs]
+
+        # Check for correct number of child matchers / slots
+        if len(self.children) > 1:
+            raise ValueError("Number of provided matchers is greater than 1.")
+
+    def f(self, c):
+        # First, filter candidates by matching splits pattern
+        if self.children:
+            for child in self.children:
+                if child.f(c) <= 0: return False
+        if not self.reverse:
+            if self.type == 'rgx':
+                for rgx in self.r:
+                    if re.search(rgx, c.get_attrib_span(self.attrib, sep=self.sep)) is None:
+                        return False 
+                return True
+            elif self.type == 'words':
+                for d in self.query:
+                    for match in re.finditer(r'\w+', c.get_attrib_span('lemmas')):
+                        if re.match(d, c[match.start(1):match.end(1)], flags = re.IGNORECASE) is None:
+                            return False
+            return True
+        else:
+            if self.type == 'rgx':
+                for rgx in self.r:
+                    if re.search(rgx, c.get_attrib_span(self.attrib, sep=self.sep)) is not None:
+                        return False 
+            elif self.type =='words':
+                for match in re.finditer(r'\w+', c.get_attrib_span('lemmas')):
+                    for d in self.query:
+                        if re.match(d, c.get_attrib_span(self.attrib)[match.start(0):match.end(0)], flags = re.IGNORECASE) is not None:
+                            return False
+            return True
+"""
+class Intersection(NgramMatcher):
+    """Takes the intersection of candidate sets returned by child operators"""
+    def f(self, c):
+       for child in self.children:
+           if child.f(c) <= 0:
+               return False
+       return True
+
+class MergeMatcher(NgramMatcher):
+    """
+    Looks at overlapping or adjacent subsets of the span.
+    Runs two child matchers on overlapping subsets, and returns true if there is some overlapping
+    where each of the children return true for their part.
+    Compare to: Concat only returns true if they are next to each other, but doesn't check overlaps.
+    """
+    def init(self):
+        self.permutations   = self.opts.get('permutations', False)
+
+    def f(self, c):
+        if len(self.children) != 2:
+            raise ValueError("MergeMatcher takes two child Matcher objects as arguments.")
+        # Iterate over candidate splits **at the word boundaries**
+        # Split 1
+        for wsplit in range(c.get_word_start()+1, c.get_word_end()+1):
+            # Split 2
+            for wsplit2 in range(c.get_word_start(), wsplit+1):
+
+                csplit = c.word_to_char_index(wsplit) - c.char_start  # NOTE the switch to **candidate-relative** char index
+                csplit2 = c.word_to_char_index(wsplit2) - c.char_start
+
+                c1 = c[:csplit]
+                c2 = c[csplit2:]
+                if self.children[0].f(c1) and self.children[1].f(c2):
+                    return True
+                if self.permutations and self.children[1].f(c1) and self.children[0].f(c2):
+                    return True
+        return False
+
+
