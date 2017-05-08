9025fb24287ddef500793c2ce905108c86ac6c9c
Jeff Pyke <pyke@stanford.edu>
2017-05-08 17:56:13 +0000

    Update tair labeler

--- small_data/create_individual.py
+++ small_data/create_individual.py
@@ -2,4 +2,6 @@ with open ("data_400.tsv") as data:
     for line in data:
         pmc_id,text = line.split("\t")
         with open("400_files/{}.txt".format(pmc_id), 'w') as doc_file:
-            doc_file.write(text.replace(". ", ". \n"))
+            doc_file.write(text) #.replace(". ", ". \n"))
+        with open("400_files/{}.ann".format(pmc_id), 'w') as ann:
+            ann.write('')
