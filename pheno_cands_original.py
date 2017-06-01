b5312cdadeca1e4189d3c7290473cd611d828281
Tara Balakrishnan <taragb@gmail.com>
2017-06-01 06:13:11 +0000

    lindsey's commits from 5/31

--- pheno_cands_original.py
+++ pheno_cands_original.py
@@ -121,14 +121,12 @@ from extended_matchers import DictionaryMatch, Concat, RegexMatchSpan, SlotFillM
 blacklist = read_blacklist()
 blacklist.extend([stem(b) for b in blacklist])
 #phenos = load_pheno_list()
-obos = load_pheno_ontology() + ['stem', 'leaves', 'phenotype', 'carpel', 'tip', 'type' ]
-patos = parse_pato(PATO_ONTOLOGY) + ['alter', 'growth', 'develop', 'affect', 'display', 'twice', 'inhibit', 'type']
+obos = load_pheno_ontology() + ['stem', 'leaves', 'phenotype', 'carpel', 'tip']
+patos = parse_pato(PATO_ONTOLOGY) + ['alter', 'growth', 'develop', 'affect', 'display', 'twice']
 
 
 PATO = DictionaryMatch(d=patos, longest_match_only=True, stemmer='porter', blacklist=blacklist)
 OBO = DictionaryMatch(d=obos, longest_match_only=True, stemmer='porter', blacklist=blacklist)
-
-G = RegexMatchSpan(rgx=r'^[A-Z]{1,4}\d+$', ignore_case=False, longest_match_only=True)#Union(RegexMatchSpan(rgx=r'^[A-Z]{1,4}\d+$', longest_match_only=True), RegexMatchSpan(rgx=r'^[A-Z][a-z]{1,2}[\d-]+$', longest_match_only=True), longest_match_only=True)
 
 NN_ADJ = RegexMatchSpan(rgx=r'^[A-Za-z-]+(ion|ment|ance|ence|ity)s?$', longest_match_only=True)
 ADJS = DictionaryMatch(d=['JJR', 'RBR'], longest_match_only=True, attrib='pos_tags')
@@ -136,24 +134,20 @@ NUMS = RegexMatchSpan(rgx=r'^CD$', longest_match_only=True, attrib='pos_tags')#S
 #NN_PHRASE = RegexMatchSpan(rgx=r'^(JJ |JJR |VBD |NN |NNS |NNP |NNPS )*(NN|NNS|NNP|NNPS)$', attrib='pos_tags', longest_match_only=False)
 NNS = RegexMatchSpan(rgx=r'^(NN|NNS|NNP|NNPS)$', attrib='pos_tags', longest_match_only=False)
 LINK_VBS = DictionaryMatch(d=['is', 'are', 'was', 'were', 'has', 'had', 'became', 'become', 'no', 'not'], longest_match_only=True, stemmer='porter')
-LINKS = DictionaryMatch(d=['IN', 'DT', 'TO', 'CC', 'WDT', 'PRP', 'WRB'], longest_match_only=True, attrib='pos_tags')#RegexMatchSpan(rgx=r'^(IN|TO|DT|CC)$', longest_match_only=True, attrib='pos_tags')
-ADVBS = DictionaryMatch(d=['RB', 'RBR', 'RBS', 'VBD', 'VBG', 'VBN', 'JJ', 'JJR', 'JJS'], longest_match_only=True, attrib='pos_tags')
+LINKS = DictionaryMatch(d=['IN', 'DT', 'TO', 'CC', 'WDT', 'PRP'], longest_match_only=True, attrib='pos_tags')#RegexMatchSpan(rgx=r'^(IN|TO|DT|CC)$', longest_match_only=True, attrib='pos_tags')
+ADVBS = DictionaryMatch(d=['RB', 'RBR', 'RBS', 'VBD', 'VBG', 'VBN', 'JJ', 'JJS'], longest_match_only=True, attrib='pos_tags')
 NN = DictionaryMatch(d=['NN', 'NNS', 'NNP', 'NNPS'], longest_match_only=True, attrib='pos_tags')
 COMPS = DictionaryMatch(d=['compared', 'contrast', 'relative', 'than', 'similar', 'different', 'same'])
 NOS = DictionaryMatch(d=['no', 'not'], longest_match_only=True)
-PREPS = DictionaryMatch(d=['IN', 'DT', 'TO', 'PRP', 'WRB'], longest_match_only=True, attrib='pos_tags')
-PREP_PHRASE = RegexMatchEach(rgx=r'(IN|TO|DT|WDT|PRP)', attrib='pos_tags', longest_match_only=True)
+PREPS = DictionaryMatch(d=['IN', 'DT', 'TO', 'PRP'], longest_match_only=True, attrib='pos_tags')
+PREP_PHRASE = RegexMatchEach(rgx=r'(IN|TO|DT|WDT)', attrib='pos_tags', longest_match_only=True)
 DETS = DictionaryMatch(d='DT|WDT|PRP', longest_match_only=True, attrib='pos_tags')
 MODS = DictionaryMatch(d=['RB', 'RBR', 'RBS', 'VBN', 'JJ', 'JJR', 'JJS'], longest_match_only=True, attrib='pos_tags')
 NN_PHRASE = Sequence(NN, MODS, required=[1,0], longest_match_only=True)#RegexMatchSpan(rgx=r'^(JJ |JJR |JJS |VBN |NN |NNS |NNP |NNPS )*(NN|NNS|NNP|NNPS)( JJ| JJR| JJS| VBN| NN| NNS| NNP| NNPS)*$', attrib='pos_tags', longest_match_only=False)
-OBO = Sequence(Union(OBO,G, longest_match_only=True), NNS, COMPS, NOS, NUMS, MODS, required=[1,0,0,0,0,0], punct='punct', links = [PREPS], longest_match_only=True)
+OBO = Sequence(OBO, NNS, COMPS, NOS, NUMS, MODS, required=[1,0,0,0,0,0], punct='punct', links = [PREPS], longest_match_only=True)
 #OBO = Union(OBO, Union(Concat(OBO, NN_PHRASE, longest_match_only=True), Concat(OBO, PATO, longest_match_only=True), longest_match_only=True))
-PATO1 = Sequence(Union(PATO, ADJS, NN_ADJ, longest_match_only = True), NUMS, ADVBS, NOS, COMPS, required=[1,0,0,0,0], punct='punct', links = [LINKS, LINK_VBS], longest_match_only=True)
-PATO2 = Sequence(DictionaryMatch(d=['JJ', 'VBN'], longest_match_only=True, attrib='pos_tags'), DictionaryMatch(d=['RB'], longest_match_only=True, attrib='pos_tags'), LINK_VBS, PATO, NN_ADJ, NUMS, ADVBS, NOS, COMPS, required=[1,1,1,0,0,0,0,0,0], punct='punct', links = [LINKS], longest_match_only=True)
-#OBO = Concat(OBO, PATO2, right_required=False, longest_match_only=True, permutations=True)
-PATO = Union(PATO1, PATO2)#PATO = Concat(PATO, NN_PHRASE, permutations=True, right_required=False, longest_match_only=True)
-OBO = Concat(OBO, PATO, right_required=False, longest_match_only=True, permutations=True)
-
+PATO = Sequence(Union(PATO, ADJS, NN_ADJ, longest_match_only = True), NUMS, ADVBS, NOS, COMPS, required=[1,0,0,0,0], punct='punct', links = [LINKS, LINK_VBS], longest_match_only=True)
+#PATO = Concat(PATO, NN_PHRASE, permutations=True, right_required=False, longest_match_only=True)
 #OBO_TAIL = Concat(OBO1, PREP_PHRASE, longest_match_only=True)
 #PATO_TAIL = Concat(PATO1, PREP_PHRASE, longest_match_only=True)
 
