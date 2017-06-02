62fdfc34e8e8649dae7d4f0b429035e6ad9190d6
Jeff Pyke <pyke@stanford.edu>
2017-06-02 17:11:10 +0000

    Add support for pg db 2

--- set_env.py
+++ set_env.py
@@ -4,10 +4,18 @@ import os
 import subprocess
 
 def set_env():
-    filename = "prod_env.sh"
+    print "Using postgres 1!"
+    source_env("prod_env.sh")
+
+def set_env2():
+    print "Using postgres 2!"
+    source_env("prod_env2.sh")
+
+def source_env(filename):
     if os.path.isfile(filename):
         command = 'env -i bash -c "source {file} && env"'.format(file=filename)
         for line in subprocess.check_output(command, shell=True).split("\n"):
             if line:
                 key, value = line.split("=")
                 os.environ[key]= value
+
